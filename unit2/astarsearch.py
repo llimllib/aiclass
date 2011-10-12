775dc04ec01435cbd6844040a15ead9c63547c37
bill <bill.mill@gmail.com>
2011-10-12 11:54:12 -0400

    actually using the 'problem' variable in the a* search

--- unit2/astarsearch.py
+++ unit2/astarsearch.py
@@ -1,24 +1,39 @@
 import random
 from romania import graph, straight_line_distance
 
-initial = ("Arad", 0)
+class RomaniaProblem(object):
+    def __init__(self, startcity, endcity):
+        self.startcity = (startcity, 0)
+        self.endcity = endcity
+
+    def actions(self, city):
+        return graph[city]
+
+    def goaltest(self, test_city):
+        return test_city == self.endcity
+
+    def pathcost(self, path):
+        g = sum(cost for city, cost in path)
+        lastcity = path[-1][0]
+        h = straight_line_distance[lastcity]
+        return g + h
 
 def tree_search(problem, remove_choice):
     #frontier is a list containing a single array, [initial]
-    frontier = [[initial]]
+    frontier = [[problem.startcity]]
     explored = set()
 
     while 1:
         if not frontier: return False
 
-        path = remove_choice(frontier)
+        path = remove_choice(frontier, problem)
         s = path[-1]
         city, cost = s
 
         explored.add(city)
 
         #if s is a goal, return path
-        if city == "Bucharest":
+        if problem.goaltest(city):
             return (path, frontier)
 
         #for a in actions
@@ -27,17 +42,18 @@ def tree_search(problem, remove_choice):
             if not ncity in explored:
                 frontier.append(path + [node])
 
-def astar(frontier):
-    cheapest = 9999999999
+def astar(frontier, problem):
+    cheapest = None
     cheapest_path = None
+
     for i, path in enumerate(frontier):
-        pathcost = sum(cost for city, cost in path) + straight_line_distance[path[-1][0]]
-        if pathcost < cheapest:
+        pathcost = problem.pathcost(path)
+        if not cheapest or pathcost < cheapest:
             cheapest = pathcost
             cheapest_path = i
 
     return frontier.pop(cheapest_path)
 
 if __name__=="__main__":
-    #print "random search: ", tree_search(None, random_choice)[0]
-    print "astar: ", tree_search(None, astar)
+    r = RomaniaProblem("Arad", "Bucharest")
+    print "astar: ", tree_search(r, astar)
